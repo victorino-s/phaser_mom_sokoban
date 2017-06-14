2373453d105d53f97ea5b3517767d4f8d81865f5
Victorino <victorino_s@outlook.com>
2017-06-15 00:38:23 +0200

    all layers loaded + player laoded, manque les animations et les collisions  + refaire le spritesheet car je l'ai niqué (! je dois en avoir une ancienne version sur l'alienware)

--- js/states/preload.js
+++ js/states/preload.js
@@ -3,7 +3,11 @@ var preloadState = {
 		this.load.image('map_tileset', '../../assets/_spritesheets/map_tileset.png');
 		this.load.tilemap('lvl1', "../../assets/maps/level1.json", null, Phaser.Tilemap.TILED_JSON);
 
-		this.load.spritesheet('player_idle_spritesheet', '../../assets/_spritesheets/girl_sp.png', 416, 454);
+		this.load.spritesheet('player_spritesheet', '../../assets/_spritesheets/girl_sp.png', 104, 113.5);
+
+		this.load.image('caisse', '../../assets/sprites/crate_02.png');
+		this.load.image('caisse_valide', '../../assets/sprites/crate_12.png');
+		this.load.image('cible', '../../assets/sprites/environment_01.png');
 	},
 
 	create: function(){
